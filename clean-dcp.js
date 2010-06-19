0645eb25fed46a7f7c11da73542f607bfa8e7a36
Jay Mendoza <jayzer@gmail.com>
2010-06-19 02:44:24 -0500

    Add comments

--- clean-dcp.js
+++ clean-dcp.js
@@ -5,16 +5,30 @@
 // ==/UserScript==
 
 $(function() {
-    $('table:first').remove();
-    $('div#userlinks').remove();
-    $('p > ins').remove();
 
+    // Banner ad
+    $('table:first').remove();
+
+    // Logos
     $('div#logostrip')
         .parent('div.borderwrap')
         .remove();
 
+    // Navigation
+    $('div#logostrip')
+        .parent('div.borderwrap')
+        .remove();
+
+    // Quick Links bar
+    $('div#userlinks').remove();
+
+    // Google ads
+    $('p > ins').remove();
+
+    // Navigation
     $('div#navstrip').css({'margin': '0', 'padding': '0'});
 
+    // Subforum listing
     $('div#fo_3')
         .insertAfter('div.borderwrap:last')
         .before('<br />');
@@ -23,24 +37,29 @@ $(function() {
         .insertAfter('div.borderwrap:last')
         .before('<br />');
 
+    // Thread listing subheaders
     $('td.darkrow1').children('b:contains("Important Topics")')
         .parents('tr').remove();
     
     $('td.darkrow1').children('b:contains("Forum Topics")')
         .parents('tr').remove();
 
+    // Pinned topics
     $('td.row1').children('div:contains("Important:")')
         .parents('tr').remove();
 
+    // Topic title
     $('td.row1 > div a')
         .filter(function() { return this.id.match(/tid-link-\d+/); })
         .css({'text-decoration': 'none',
               'font-size': '12px'});
 
+    // Topic subtitle
     $('div.desc > span')
         .filter(function() { return this.id.match(/tid-desc-\d+/); })
         .css({'color': '#888'});
 
+    // Thread icons
     $('th:contains("Topic Title")')
         .prev().remove();
 
@@ -48,6 +67,7 @@ $(function() {
         .filter(function() { return this.id.match(/tid-folder-\d+/); })
         .next().remove();
 
+    // Line breaks
     $('script[src="jscripts/ipb_forum.js"]').next('br').remove();
     $('table.ipbtable').prev('br').remove();
 });
